7b6a1a8a3417fa8870b40a70a1ac94b9a08aec7a
Sujan Maji <dev.sujanmaji@gmail.com>
2023-08-28 12:57:09 +0530

    auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className='text-3xl font-bold text-indigo-500'>Discord Clone</h1>
+      <h1 className="text-3xl font-bold text-indigo-500">Discord Clone</h1>
       <Button>Test</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
+  );
 }
